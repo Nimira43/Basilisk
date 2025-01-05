bfc7f104a57a4f4f3565f3138738614af07d1378
Nimira43 <n.rabone72@aol.com>
2025-01-05 12:31:34 +0000

    Corrected  mistake - socket.io installed. Server now running

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const { Server } = require('socket.io')
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, console.log(`Server running on Port ${PORT}`))
