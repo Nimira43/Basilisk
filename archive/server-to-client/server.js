22262622276d08b3ef367f68d1d2b2603b6e946e
Nimira43 <n.rabone72@aol.com>
2025-01-05 14:01:36 +0000

    Archived first demo

---
+++ archive/server-to-client/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const { join } = require('path')
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'))
+})
+
+io.on('connection', (socket)=> {
+  console.log('A user has been connected')
+  socket.emit('messageFromServer', 'Hello User - This is the Server')
+})
+
+server.listen(PORT, console.log(`Server running on Port ${PORT}`))
