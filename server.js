a895e8dbb887e8a709d0ec14cb39e3b6bb3f9a11
Nimira43 <n.rabone72@aol.com>
2025-01-05 13:57:10 +0000

    Now emitting message to user upon connection

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket)=> {
   console.log('A user has been connected')
+  socket.emit('messageFromServer', 'Hello User - This is the Server')
 })
 
 server.listen(PORT, console.log(`Server running on Port ${PORT}`))
