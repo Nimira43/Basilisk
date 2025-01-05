39393ac3f664809bbed401dfaccb961aa674fe2a
Nimira43 <n.rabone72@aol.com>
2025-01-05 12:36:55 +0000

    With the socket parameter in the connection event handler, it should log the socket object and a message when a user connects.

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ const server = createServer(app)
 const io = new Server(server)
 const PORT = process.env.PORT || 3000
 
+io.on('connection', (socket)=> {
+  console.log(socket)
+  console.log('A user has been connected')
+})
+
 server.listen(PORT, console.log(`Server running on Port ${PORT}`))
