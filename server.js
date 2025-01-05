7d60c4c9a721cfb146fee94501449f279165ea7c
Nimira43 <n.rabone72@aol.com>
2025-01-05 12:57:55 +0000

    Serving index.html and handling socket connections.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
+const { join } = require('path')
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
 const PORT = process.env.PORT || 3000
 
-app.get.get('/', (req))
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'))
+})
 
 io.on('connection', (socket)=> {
   console.log(socket)
