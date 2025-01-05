497fb31d4caf055e964668a7847d6574e1ff16be
Nimira43 <n.rabone72@aol.com>
2025-01-05 11:46:00 +0000

    Setting up Express server witth Socket.io for handling real time communication

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
